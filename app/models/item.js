329d55bfd14bc1a872f4eaf2a2da17bf9737ce97
santiago.sanmartin <santiago.sanmartin@udea.edu.co>
2018-03-10 12:03:28 -0500

    final

---
+++ app/models/item.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+
+export default DS.Model.extend({
+   description: DS.attr("string"),
+   //belongsTo  es decir que el item pertenece a una lista
+   list: DS.belongsTo("list")
+});
